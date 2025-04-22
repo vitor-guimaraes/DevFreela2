d421b938a4391e6e6b8c059ab31fcd964871e33d
Vitor-Guimaraes <guimvitor@gmail.com>
2025-04-22 07:05:45 -0300

    Inserted options to appsettings

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -1,6 +1,7 @@
 ﻿using DevFreela.API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace DevFreela.API.Controllers
 {
@@ -8,6 +9,12 @@ namespace DevFreela.API.Controllers
     [ApiController]
     public class ProjectsController : ControllerBase
     {
+        private readonly FreelanceTotalCostConfig _config;
+        public ProjectsController(IOptions<FreelanceTotalCostConfig> options)
+        {
+            _config = options.Value;
+        }
+
         [HttpGet]
         public IActionResult GetAll()
         {
@@ -23,6 +30,10 @@ namespace DevFreela.API.Controllers
         [HttpPost]
         public IActionResult Post(CreateProjectInputModel model)
         {
+            if (model.TotalCost <= _config.Minimum || model.TotalCost > _config.Maximum)
+            {
+                return BadRequest();
+            }
             return CreatedAtAction(nameof(GetById), new { id = 1 }, model);
         }
 
