396c76cad02653d763f491e9debaff570fadb123
Vitor-Guimaraes <guimvitor@gmail.com>
2025-07-01 20:50:55 -0300

    Created entities and fixed connection string

---
+++ DevFreela.API/Entities/Skill.cs
@@ -0,0 +1,15 @@
+﻿namespace DevFreela.API.Entities
+{
+    public class Skill : BaseEntity
+    {
+        protected Skill()
+        {
+        }
+        public Skill(string description)
+            : base()
+        {
+            Description = description;
+        }
+        public string Description { get; private set; }
+    }
+}
