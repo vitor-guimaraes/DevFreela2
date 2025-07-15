6532fef7d252858a94429a3e7fb971955f1b29a9
Vitor-Guimaraes <guimvitor@gmail.com>
2025-07-15 20:11:45 -0300

    Updated GetAll search method

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -26,7 +26,8 @@ namespace DevFreela.API.Controllers
             var projects = _context.Projects
                 .Include(p => p.Client)
                 .Include(p => p.Freelancer)
-                .Where(p => !p.IsDeleted).ToList();
+                .Where(p => !p.IsDeleted && (search == "" || p.Title.Contains(search) || p.Description.Contains(search)))
+                .ToList();
             
             var model = projects.Select(ProjectItemViewModel.FromEntity).ToList();
 
