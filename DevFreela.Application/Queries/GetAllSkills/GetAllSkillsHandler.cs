e0c21f4318d4e50d9611601cd76b4786f4fd6696
Vitor-Guimaraes <guimvitor@gmail.com>
2025-10-05 23:36:19 -0300

    Implemented Queries and Handlers

---
+++ DevFreela.Application/Queries/GetAllSkills/GetAllSkillsHandler.cs
@@ -0,0 +1,37 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Infrastucture.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Queries.GetAllSkills
+{
+    public class GetAllSkillsHandler : IRequestHandler<GetAllSkillsQuery, ResultViewModel<List<SkillViewModel>>>
+    {
+        private readonly DevFreelaDbContext _context;
+        public GetAllSkillsHandler(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ResultViewModel<List<SkillViewModel>>> Handle(GetAllSkillsQuery request, CancellationToken cancellationToken)
+        {
+            var skills = await _context.Skills.ToListAsync();
+
+            if (skills == null || skills.Count == 0)
+            {
+                return ResultViewModel<List<SkillViewModel>>.Error("No skills found");
+            }
+
+            var model = skills.ToList()
+                        .Select(s => new SkillViewModel(s.Id, s.Description))
+                        .ToList();
+
+            return ResultViewModel<List<SkillViewModel>>.Success(model);
+        }
+    }
+}
---
+++ DevFreela.Application/Queries/GetSkillById/GetSkillByIdHandler.cs
@@ -0,0 +1,34 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Infrastucture.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Queries.GetSkillById
+{
+    public class GetSkillByIdHandler : IRequestHandler<GetSkillByIdQuery, ResultViewModel<SkillViewModel>>
+    {
+        private readonly DevFreelaDbContext _context;
+
+        public GetSkillByIdHandler(DevFreelaDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<ResultViewModel<SkillViewModel>> Handle(GetSkillByIdQuery request, CancellationToken cancellationToken)
+        {
+            var skill = await _context.Skills.SingleOrDefaultAsync(s => s.Id == request.Id);
+
+            if (skill == null)
+            {
+                return ResultViewModel<SkillViewModel>.Error("Skill not found");
+            }
+
+            var model = new SkillViewModel(skill.Id, skill.Description);
+            return ResultViewModel<SkillViewModel>.Success(model);
+        }
+    }
+}
