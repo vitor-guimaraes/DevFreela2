60e474b2cdf5066c25025e8c087cdd642d7fb70d
Vitor-Guimaraes <guimvitor@gmail.com>
2025-10-05 23:36:19 -0300

    Implemented Queries and Handlers for Skills

---
+++ DevFreela.Application/Queries/GetSkillById/GetSkillByIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using DevFreela.Application.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Queries.GetSkillById
+{
+    public class GetSkillByIdQuery : IRequest<ResultViewModel<SkillViewModel>>
+    {
+        public int Id { get; set; }
+
+        public GetSkillByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
